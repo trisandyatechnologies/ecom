f16d31519721fdad572c56fde2330627ffbd57e6
Ganesh <saiganesharaja@gmail.com>
2024-01-02 10:43:42 +0530

    hi friends

--- src/app/components/Rating.tsx
+++ src/app/components/Rating.tsx
@@ -1,57 +1,52 @@
 'use client';
 import React, { useState } from 'react';
-import { Col, InputNumber, Row, Slider, Space, Typography } from 'antd';
+import { Col, Flex, Row, Space, Typography } from 'antd';
 import { StarOutlined } from '@ant-design/icons';
+import { Progress } from 'antd';
 
 
 const IntegerStep: React.FC = () => {
-  const [inputValue, setInputValue] = useState(1);
-
-  const onChange = (newValue: number) => {
-    setInputValue(newValue);
-  };
+ 
 
   return (
-    <Row style={{marginTop:'20%'}}>
+    
+    <Row style={{padding:"200px"}}>
      
-      <Col span={8}>
+      <Col span={6}>
        
-    <Typography style={{color:"green",fontSize:'50px'}}>3.5 <StarOutlined /></Typography>
+    <Typography style={{color:"green",fontSize:'40px'}}>3.5 <StarOutlined style={{color:'green'}}/></Typography>
     <Typography style={{border:'none'}}>5464 Ratings</Typography>
     <Typography>243534 Reviews</Typography>
         
       </Col>
-      <Col span={6}>
-        
-        <Slider
-    
-    
-        
-          defaultValue={30}
-        />
-      
-      
-        <Slider
-          min={3}
-          
-      defaultValue={40}
-        />
-          <Slider
-    
-    
-        
-    defaultValue={30}
-  />
+      <Col style={{width:'450px'}}>
 
+         <>
+         
+     
+    <Col span={18} style={{ display: "flex", flexDirection: "row", alignItems: "center", width:"370px", gap:"4px"}}>
+      <Typography style={{ width: '70%', textAlign: 'center',padding:'5px 5px' }}>Excellent</Typography>
+      <Progress percent={91} strokeColor="green" style={{width:"900px"}}/>
+    </Col>
+    <Col span={18} style={{ display: "flex", flexDirection: "row", alignItems: "center", width:"370px", gap:"4px"}}>
+      <Typography style={{ width: '70%', textAlign: 'center',padding:'5px 5px' }}>Very good</Typography>
+      <Progress percent={40} strokeColor="green" style={{width:"900px"}}/>
+    </Col>
+    <Col span={18} style={{ display: "flex", flexDirection: "row", alignItems: "center", width:"370px", gap:"4px"}}>
+      <Typography style={{ width: '70%', textAlign: 'center',padding:'5px 5px' }}>Good</Typography>
+      <Progress percent={45} strokeColor="yellow" style={{width:"900px"}}/>
+    </Col>
+    <Col span={18} style={{ display: "flex", flexDirection: "row", alignItems: "center", width:"370px", gap:"4px"}}>
+      <Typography style={{ width: '70%', textAlign: 'center',padding:'5px 5px' }}>Average</Typography>
+      <Progress percent={22} strokeColor="orange" style={{width:"900px"}}/>
+    </Col>
+    <Col span={18} style={{ display: "flex", flexDirection: "row", alignItems: "center", width:"370px", gap:"4px"}}>
+      <Typography style={{ width: '70%', textAlign: 'center',padding:'5px 5px' }}>Poor</Typography>
+      <Progress percent={31} strokeColor="red" style={{width:"900px"}}/>
+    </Col>
+  </>
 
-  <Slider
-  style={{color:'red'}}
-    min={3}
-    
-    
-defaultValue={40}
-
-  />
+  
       </Col>
     
     </Row>
