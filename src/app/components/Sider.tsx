22263b58a50ae44905f60bd7497b6d22398e4ca0
tulasi Kurete <tulasikurete410@gmail.com>
2024-01-03 10:40:11 +0530

    small changes

--- src/app/components/Sider.tsx
+++ src/app/components/Sider.tsx
@@ -34,116 +34,109 @@ export default function Sider() {
   const onChange = (checkedValues: CheckboxValueType[]) => {
     console.log("checked = ", checkedValues);
   };
-  function callback() {
-    // console.log(key);
-  }
 
   const { Panel } = Collapse;
   return (
     <main className="sideresponsive">
       <h1>Products For You</h1>
-      <section>
-        <section>
-          <Space wrap>
-            <Select
-              defaultValue="Sort by :"
-              style={{ width: 90}}
-              onChange={handleChange}
-              options={[
-                { value: "Relevance", label: "Sort by: Relevance" },
-                { value: "New Arrivals", label: "New Arrivals" },
-                { value: "Price (High to Low)", label: "Price (High to Low)" },
-                { value: "Price (Low to High)", label: "Price (Low to High)" },
-                { value: "Ratings", label: "Ratings" },
 
-                { value: "Discount", label: "Discount", disabled: true },
-              ]}
-            />
+      <Space wrap>
+        <Select
+          defaultValue="Sort by :"
+          style={{ width: 90 }}
+          onChange={handleChange}
+          options={[
+            { value: "Relevance", label: "Sort by: Relevance" },
+            { value: "New Arrivals", label: "New Arrivals" },
+            { value: "Price (High to Low)", label: "Price (High to Low)" },
+            { value: "Price (Low to High)", label: "Price (Low to High)" },
+            { value: "Ratings", label: "Ratings" },
+
+            { value: "Discount", label: "Discount", disabled: true },
+          ]}
+        />
+      </Space>
+
+      <Divider />
+
+      <Space>
+        <Typography>FILTERS</Typography>
+        <Typography>1000+ Products</Typography>
+      </Space>
+
+      <Divider />
+
+      <Space>
+        <Typography>Category</Typography>
+      </Space>
+      <Flex>
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="search"
+          // defaultValue={['apple']}
+
+          onChange={onChange}
+          optionLabelProp="label"
+          options={options}
+        />
+      </Flex>
+
+      <Collapse
+        expandIconPosition="right"
+        defaultActiveKey={["1"]}
+        onChange={callback}
+        style={{ borderTop: "1px solid grey", borderRadius: 0 }}
+        ghost
+      >
+        <Panel header="color" key="1">
+          <Space style={{ flexWrap: "wrap" }}>
+            <Button>Multicolor</Button>
+            <Button>purple</Button>
+            <Button>white</Button>
+            <Button>Black</Button>
+            <Button>orange</Button>
+            <Button>red</Button>
+            <Button>Blue</Button>
           </Space>
-        </section>
-        <section>
-          <Divider />
+        </Panel>
+      </Collapse>
 
-          <Space>
-            <Typography>FILTERS</Typography>
-            <Typography>1000+ Products</Typography>
+      <Collapse
+        expandIconPosition="right"
+        defaultActiveKey={["1"]}
+        onChange={callback}
+        style={{ borderTop: "1px solid grey", borderRadius: 0 }}
+        ghost
+      >
+        <Panel header="Price" key="1">
+          <Space style={{ flexWrap: "wrap" }}>
+            <Button>under $ 234</Button>
+            <Button>under $ 111</Button>
+            <Button>under $ 456</Button>
+            <Button>under $ 150</Button>
           </Space>
+        </Panel>
+      </Collapse>
 
-          <Divider />
-
-          <Space>
-            <Typography>Category</Typography>
-          </Space>
-          <Flex>
-            <Select
-              mode="multiple"
-              style={{ width: "100%" }}
-              placeholder="search"
-              // defaultValue={['apple']}
-
-              onChange={onChange}
-              optionLabelProp="label"
-              options={options}
-            />
-          </Flex>
-
-          <Collapse
-            expandIconPosition="right"
-            defaultActiveKey={["1"]}
-            onChange={callback}
-            style={{ borderTop: "1px solid grey", borderRadius: 0 }}
-            ghost
-          >
-            <Panel header="color" key="1">
-              <Space style={{ flexWrap: "wrap" }}>
-                <Button>Multicolor</Button>
-                <Button>purple</Button>
-                <Button>white</Button>
-                <Button>Black</Button>
-                <Button>orange</Button>
-                <Button>red</Button>
-                <Button>Blue</Button>
-              </Space>
-            </Panel>
-          </Collapse>
-
-          <Collapse
-            expandIconPosition="right"
-            defaultActiveKey={["1"]}
-            onChange={callback}
-            style={{ borderTop: "1px solid grey", borderRadius: 0 }}
-            ghost
-          >
-            <Panel header="Price" key="1">
-              <Space style={{ flexWrap: "wrap" }}>
-                <Button>under $ 234</Button>
-                <Button>under $ 111</Button>
-                <Button>under $ 456</Button>
-                <Button>under $ 150</Button>
-              </Space>
-            </Panel>
-          </Collapse>
-
-          <Collapse
-            expandIconPosition="right"
-            defaultActiveKey={["1"]}
-            onChange={callback}
-            ghost
-            style={{
-              borderTop: "1px solid grey",
-              borderRadius: 0,
-            }}
-          >
-            <Panel header="Combo" key="1">
-              <Checkbox.Group
-                options={plainOptions}
-                defaultValue={["combos"]}
-                onChange={onChange}
-              />
-            </Panel>
-          </Collapse>
-        </section>
-      </section>
+      <Collapse
+        expandIconPosition="right"
+        defaultActiveKey={["1"]}
+        onChange={callback}
+        ghost
+        style={{
+          borderTop: "1px solid grey",
+          borderRadius: 0,
+        }}
+      >
+        <Panel header="Combo" key="1">
+          <Checkbox.Group
+            options={plainOptions}
+            defaultValue={["combos"]}
+            onChange={onChange}
+          />
+        </Panel>
+      </Collapse>
     </main>
   );
 }
