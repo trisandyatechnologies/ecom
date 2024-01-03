369b49c2fe10b9671874f091ee6ab3b0bddb0e82
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2024-01-03 11:44:39 +0530

    productpage-updated

--- src/app/components/ProductPage/Page.tsx
+++ src/app/components/ProductPage/Page.tsx
@@ -1,5 +1,6 @@
 import { DoubleRightOutlined, InfoCircleOutlined, PlaySquareTwoTone, RocketTwoTone, ShoppingCartOutlined, StarFilled, StarOutlined, TabletTwoTone, ThunderboltTwoTone } from "@ant-design/icons";
 import { Button, Col, Flex, Rate, Row, Space, Typography } from "antd";
+import "../.././globals.css";
 
 const ProductPage: React.FC = () => {
     return (
@@ -19,30 +20,30 @@ const ProductPage: React.FC = () => {
                     <Space>
                         <img src="https://images.meesho.com/images/products/21914499/83fac_512.webp" style={{ width: "100%" }} />
                     </Space>
-                    <Flex justify="center" style={{ width: "100%", marginTop:"20px",padding: "0 30px", gap: "10px" }}>
+                    <Flex justify="center" style={{ width: "100%", marginTop: "20px", padding: "0 30px", gap: "10px" }}>
                         <Button style={{ color: "purple", width: "200px", borderRadius: "5px", border: "1px solid purple", fontSize: "18px", height: "50px" }}><ShoppingCartOutlined />Add to Cart</Button>
                         <Button style={{ background: "purple", width: "200px", borderRadius: "5px", color: "white", fontSize: "18px", height: "50px" }}><DoubleRightOutlined />Buy Now</Button>
                     </Flex>
                 </Col>
-                
-                <Col span={8} style={{ margin: "30px" }}>
-                    <Flex style={{ flexDirection: "column", border: "0.5px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px" }}>
+
+                <Col span={8} style={{ margin: "30px" }} className="product_details">
+                    <Flex className="first_column" style={{ flexDirection: "column", border: "0.5px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px" }}>
                         <Typography>STLYZ Polyresin Baby Hat Monk Buddha Idols Standard Multicolour, 4 Pieces-Home Decor</Typography>
-                        <Typography style={{ display: "flex", gap: "5px" }}><Typography style={{ fontSize: "24px" }}>₹ 278 </Typography><InfoCircleOutlined /></Typography>
-                        <Typography style={{ display: "flex", gap: "10px" }}>
+                        <Typography style={{ display: "flex", gap: "5px" }} id="rate"><Typography style={{ fontSize: "24px" }}>₹ 278 </Typography><InfoCircleOutlined /></Typography>
+                        <Typography style={{ display: "flex", gap: "10px" }} id="rating">
                             <Typography style={{ display: "flex", borderRadius: "25px", width: "50px", background: "rgb(3, 141, 99)", color: "white", justifyContent: "center", gap: "4px" }}>4.2 <StarFilled /> </Typography>
                             5402 Ratings, 1295 Reviews
                         </Typography>
-                        <Typography style={{ display: "flex", marginTop: "10px", justifyContent:"center",borderRadius: "10px", width: "110px", background: "rgb(245 245 245)", color: "black", gap: "4px" }}>Free Delivery  </Typography>
+                        <Typography id="free_delivery" style={{ display: "flex", marginTop: "10px", justifyContent: "center", borderRadius: "10px",maxWidth: "130px", background: "rgb(245 245 245)", color: "black", gap: "4px" }}>Free Delivery  </Typography>
 
                     </Flex>
-                    <Flex style={{ flexDirection: "column", border: "0.5px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px", margin: "15px auto" }}>
+                    <Flex className="secoend_column" style={{ flexDirection: "column", border: "0.5px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px", margin: "15px auto" }}>
                         <Typography style={{ fontSize: "24px" }}>Select Size</Typography>
 
                         <Typography style={{ display: "flex", marginTop: "10px", border: "1px solid rgb(159, 32, 137)", borderRadius: "10px", width: "100px", justifyContent: "center", background: "rgb(255, 231, 251)", color: "rgb(159, 32, 137)", gap: "4px" }}>Free Size  </Typography>
 
                     </Flex>
-                    <Flex style={{ flexDirection: "column", border: "0.8px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px" }}>
+                    <Flex className="third_column" style={{ flexDirection: "column", border: "0.8px solid gray", background: "white", borderRadius: "5px", gap: "10px", padding: "30px" }}>
                         <Typography>Name : STLYZ Polyresin Baby Hat Monk Buddha Idols Standard Multicolour, 4 Pieces-Home Decor</Typography>
                         <Typography style={{ fontSize: "12px" }}>Material : Poly Resin </Typography>
                         <Typography style={{ fontSize: "12px" }}>Type : Car </Typography>
@@ -52,10 +53,6 @@ const ProductPage: React.FC = () => {
 
                         <Typography style={{ fontSize: "12px" }}>Country of Origin : India </Typography>
                         <Typography style={{ fontSize: "12px", borderBottom: "0.8px dashed rgb(97, 97, 115)", width: "100px" }}>More Information </Typography>
-
-
-
-
                     </Flex>
                 </Col>
                 <Col span={2}></Col>
