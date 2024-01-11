109d37bdcd267581e744193921190fe923bdca9c
tulasi Kurete <tulasikurete410@gmail.com>
2024-01-11 13:14:35 +0530

    update file

--- src/app/admin/items/new/page.tsx
+++ src/app/admin/items/new/page.tsx
@@ -12,18 +12,30 @@ const onFinish = (values: any) => {
 
 const NewItem: React.FC = () => (
   <Flex>
-    <Form name="nest-messages" onFinish={onFinish} style={{ width: "100%" }}>
+    <Form
+      name="nest-messages"
+      onFinish={onFinish}
+      style={{ width: "100%" }}
+      layout="vertical"
+    >
       <Row gutter={16}>
-        <Col xs={24} lg={12}>
+        <Col xs={12} lg={12}>
           <Form.Item name="name" label="Name" rules={[{ required: true }]}>
             <Input />
           </Form.Item>
         </Col>
-        <Col xs={24} lg={12}>
-          <Form.Item name="images">
-            <ImageUpload />
+        <Col xs={12} lg={12}>
+          <Form.Item label="Category" name="category">
+            <Select>
+              {categoryItems.map(({ key, label }) => (
+                <Select.Option key={key} value={key}>
+                  {label}
+                </Select.Option>
+              ))}
+            </Select>
           </Form.Item>
         </Col>
+
         <Col xs={12} lg={8}>
           <Form.Item name="price" label="Price" rules={[{ required: true }]}>
             <InputNumber placeholder="Enter Price" style={{ width: "100%" }} />
@@ -39,57 +51,83 @@ const NewItem: React.FC = () => (
         </Col>
         <Col xs={12} lg={8}>
           <Form.Item label="Quantity" name="quantity">
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} lg={12}>
+          <Form.Item label="Description" name="description">
+            <Input.TextArea />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} lg={12}>
+          <Form.Item name="images">
+            <ImageUpload />
+          </Form.Item>
+        </Col>
+
+        <Col xs={12} lg={12}>
+          <Form.Item label="Brand" name="brand">
+            <Input />
+          </Form.Item>
+        </Col>
+
+        <Col xs={12} lg={12}>
+          <Form.Item label="Sold By" name={["details", "soldBy"]}>
+            <Input />
+          </Form.Item>
+        </Col>
+        
+        <Col xs={12} lg={8}>
+          <Form.Item label="Type" name={["details", "type"]}>
+            <Input />
+          </Form.Item>
+        </Col>
+
+        <Col xs={12} lg={8}>
+          <Form.Item label="Capacity" name={["details", "capacity"]}>
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Col>
+        <Col xs={12} lg={8}>
+          <Form.Item label="Origin" name={["details", "origin"]}>
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col xs={12} lg={8}>
+          <Form.Item label="Weight" name={["details", "weight"]}>
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Col>
+        <Col xs={24} lg={8}>
+          <Form.Item label="Units" name={["details", "units"]}>
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Col>
+        <Col xs={12} lg={8}>
+          <Form.Item label="Size" name={["details", "size"]}>
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} lg={12}>
+          <Form.Item label="Pattern" name={["details", "pattern"]}>
+            <Input />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} lg={12}>
+          <Form.Item label="Flavour" name={["details", "flavour"]}>
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col xs={24} lg={12}>
+          <Form.Item label="Style" name={["details", "style"]}>
             <Input />
           </Form.Item>
         </Col>
       </Row>
-      <Form.Item label="Category" name="category">
-        <Select>
-          {categoryItems.map(({ key, label }) => (
-            <Select.Option key={key} value={key}>
-              {label}
-            </Select.Option>
-          ))}
-        </Select>
-      </Form.Item>
-      <Form.Item label="Description" name="description">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Brand" name="brand">
-        <Input />
-      </Form.Item>
-
-      <Form.Item label="Sold By" name={["details", "soldBy"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Size" name={["details", "size"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Type" name={["details", "type"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Capacity" name={["details", "capacity"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Origin" name={["details", "origin"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Weight" name={["details", "weight"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Pattern" name={["details", "pattern"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Units" name={["details", "units"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Flavour" name={["details", "flavour"]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Style" name={["details", "style"]}>
-        <Input />
-      </Form.Item>
-
       <Form.Item>
         <Button type="primary" htmlType="submit">
           Submit
