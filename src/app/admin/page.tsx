d79aeda4d2fdd39b53535ab2828613ce8d9cec19
Naga Durga <gollanagadurga16@gmail.com>
2024-01-11 10:16:09 +0530

    dashboard files

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,115 +1,32 @@
-"use client";
-import React, { useState } from 'react';
-import { Button, Dropdown, Flex, Form, Input, InputNumber, MenuProps, Select, Space } from 'antd';
-import { addItem } from '@/lib/api';
-import ImageUpload from '@/components/ImageUpload';
-import { DownOutlined } from '@ant-design/icons';
-import { Category } from '@prisma/client';
+'use client'
+import React from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+import Admin from './items/new_items/page';
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
-  },
-  number: {
-    range: '${label} must be between ${min} and ${max}',
-  },
+const onChange = (key: string) => {
+  console.log(key);
 };
 
-
-
-const onFinish = (values: any) => {
-  console.log(values);
- addItem(values);
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: 'Items',
+    children: 'Content of Tab Pane 1',
+  },
   
-};
+  {
+    key: '2',
+    label: 'NewItem',
+    children: <Admin/>,
+  },
+  {
+    key: '3',
+    label: 'Orders',
+    children: 'Content of Tab Pane 2',
+  },
+];
 
+const App: React.FC = () => <Tabs defaultActiveKey="1" items={items} onChange={onChange} tabPosition="left" />;
 
-
-const Admin: React.FC = () => (
-  <Flex >
-  <Form
-    {...layout}
-    name="nest-messages"
-    onFinish={onFinish}
-    style={{ maxWidth: 600}}
-    validateMessages={validateMessages}
-    
-  >
-    <Form.Item name='name' label="Item Name" rules={[{ required: true }]}>
-      <Input />
-    </Form.Item>
-    <Form.Item name='images'>
-    <ImageUpload/>
-    </Form.Item>
-    <Form.Item name='price' label="Item Price" rules={[{ required: true }]} >
-        <InputNumber placeholder="Enter Price" type='number' />
-      </Form.Item>
-      <Form.Item name='mrp' label="Item MRP" rules={[{ required: true }]} >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item label="Discount" name='discount'>
-        <InputNumber  />
-      </Form.Item>
-      <Form.Item label="Category" name='category'>
-        <Select>
-          {
-            Object.keys(Category).map(cat => <Select.Option value={cat}>{cat.replaceAll('_', ' ')}</Select.Option>)
-          }
-        </Select>
-      </Form.Item>
-      <Form.Item label="Description" name='description'>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Brand" name='brand'>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Quantity" name='quantity'>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Sold By"name={['details', 'soldBy']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Size" name={['details', 'size']}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Type" name={['details', 'type']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Capacity" name={['details', 'capacity']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Origin" name={['details', 'origin']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Weight" name={['details', 'weight']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Pattern" name={['details', 'pattern']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Units" name={['details', 'units']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Flavour" name={['details', 'flavour']}>
-        <Input  />
-      </Form.Item>
-      <Form.Item label="Style" name={['details', 'style']}>
-        <Input  />
-      </Form.Item>
-    
-    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-  </Flex>
-);
-
-export default Admin;
+export default App;
