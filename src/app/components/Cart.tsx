f817e9a6c4d35117cb1969b81ac70e9c38deee36
tulasi Kurete <tulasikurete410@gmail.com>
2024-01-12 15:14:26 +0530

    modified cartpage

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,129 +1,20 @@
 "use client";
-import React, { useState } from "react";
-import { Affix, Button, Divider, Flex, Space, Typography } from "antd";
+import React from "react";
+import { Affix, Button, Divider, Flex, Typography } from "antd";
 import { Col, Row } from "antd";
-import { Steps, theme } from "antd";
-import { Avatar, Card, Skeleton, Switch } from "antd";
+import { theme } from "antd";
+import { Avatar, Card } from "antd";
+import { useCartStore } from "@/lib/cartStore";
+import { getThumbnail } from "@/utils/util";
 
-const steps = [
-  {
-    title: "Cart",
-    content: "First-content",
-  },
-  {
-    title: "Address",
-    content: "Second-content",
-  },
-  {
-    title: "Payment",
-    content: "Last-content",
-  },
-  {
-    title: "Summary",
-    content: "Last-content",
-  },
-];
-const CartItem = [
-  {
-    image: "https://images.meesho.com/images/products/308205375/cd8oh_400.webp",
-    name: "Kids baby sofa seat and sofa cum bed for 1 to 2 year ",
-    price: 100,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/222754448/ilwoy_400.webp",
-    name: "Kids Educational Velvet Learning Baby Pillow Cushion Soft Book",
-    price: 400,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/46618622/5bqvz_512.webp",
-    name: "Teddy Bear ",
-    price: 200,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/308205375/cd8oh_400.webp",
-    name: "Kids baby sofa seat and sofa cum bed for 1 to 2 year ",
-    price: 100,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/222754448/ilwoy_400.webp",
-    name: "Kids Educational Velvet Learning Baby Pillow Cushion Soft Book",
-    price: 400,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/46618622/5bqvz_512.webp",
-    name: "Teddy Bear ",
-    price: 200,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/308205375/cd8oh_400.webp",
-    name: "Kids baby sofa seat and sofa cum bed for 1 to 2 year ",
-    price: 100,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/222754448/ilwoy_400.webp",
-    name: "Kids Educational Velvet Learning Baby Pillow Cushion Soft Book",
-    price: 400,
-    size: "large",
-    Qty: 1,
-  },
-  {
-    image: "https://images.meesho.com/images/products/46618622/5bqvz_512.webp",
-    name: "Teddy Bear ",
-    price: 200,
-    size: "large",
-    Qty: 1,
-  },
-];
 const { Meta } = Card;
-export default function Cart() {
+export default function CartPage() {
   const { token } = theme.useToken();
-  const [current, setCurrent] = useState(0);
 
-  const items = steps.map((item) => ({ key: item.title, title: item.title }));
-
-  const contentStyle: React.CSSProperties = {
-    color: token.colorTextTertiary,
-    backgroundColor: token.colorFillAlter,
-    borderRadius: token.borderRadiusLG,
-    border: `1px dashed ${token.colorBorder}`,
-    marginTop: 16,
-  };
-  const [loading, setLoading] = useState(true);
-
+  const cart = useCartStore((s) => s.cart);
+  const cartItems = Object.values(cart);
   return (
     <main style={{ background: "white" }}>
-      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-        <Col span={12}>
-          <Typography.Title>meesho</Typography.Title>
-        </Col>
-
-        <Col span={12}>
-          <Steps
-            current={current}
-            size="small"
-            labelPlacement="vertical"
-            items={items}
-            style={{ marginTop: 15 }}
-          />
-        </Col>
-      </Row>
-
-      <Divider />
-
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         <Col lg={12} xs={24}>
           <Flex>
@@ -132,18 +23,18 @@ export default function Cart() {
               type="vertical"
               style={{ borderLeft: "1px solid black", height: "30px" }}
             />
-            <Typography> {CartItem.length}Items</Typography>
+            <Typography> {cartItems.length} Items</Typography>
           </Flex>
-          {CartItem.map(({ image, name, price, size, Qty }, i) => (
+          {cartItems.map((item, i) => (
             <Card
+              key={item.id}
               style={{
                 background: "white",
                 border: "1px solid black",
                 margin: 20,
               }}
               actions={[
-                <Typography>Sold by : Srinu Enterprises</Typography>,
-                <Typography>Free Delivery</Typography>,
+                <Typography> Above 500/- purchase Free Delivery</Typography>,
               ]}
             >
               <Flex vertical>
@@ -155,29 +46,23 @@ export default function Cart() {
                   }}
                   avatar={
                     <Avatar
-                      src={image}
+                      src={getThumbnail(item.image)}
                       style={{ width: 100, height: 100, borderRadius: 0 }}
                     />
                   }
-                  title={name}
-                  description={price}
+                  title={item.name}
+                  description={item.price}
                 />
               </Flex>
               <Typography
                 style={{ display: "flex", justifyContent: "flex-end" }}
               >
-                Edit
+                Update Quantity
               </Typography>
               <Typography>All issue easy returns allowed</Typography>
               <Flex gap={10}>
-                <Typography> Size : {size}</Typography>
-                <Typography>Qty : {Qty}</Typography>
+                <Typography>Qty : {item.quantity}</Typography>
               </Flex>
-              {
-                <Button style={{ border: "none", fontSize: 20 }}>
-                  x Remove
-                </Button>
-              }
             </Card>
           ))}
         </Col>
