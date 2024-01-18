95b3d92d79a6b92aa5f6bbc6cd7c7e41cc758eb2
tulasi Kurete <tulasikurete410@gmail.com>
2024-01-18 15:30:37 +0530

    modified

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import Profile from "@/components/UserProfile";
-=======
 import Profile from "@/components/EditProfile";
 import Link from "next/link";
->>>>>>> main
 
 export default async function Account() {
   return (
     <main>
-<<<<<<< HEAD
-      <Profile />
-=======
       <h2>User Details</h2>
       <Link href="/account/edit">Edit Details</Link>
->>>>>>> main
     </main>
   );
 }
