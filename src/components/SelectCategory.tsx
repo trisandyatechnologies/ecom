5ab4f3b99fae9d891686dba5420bc308a423fddd
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2024-01-29 15:05:18 +0530

    resolved error in Select categories

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -3,9 +3,6 @@ import React, { useEffect, useState } from "react";
 import { Select, Space, Typography } from "antd";
 import { getCategories } from "@/lib/api";
 
-const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
-};
 
 const SelectCategory: React.FC = () => {
   const [options, setOptions] = useState();
@@ -13,20 +10,26 @@ const SelectCategory: React.FC = () => {
     getCategories().then((catagories) => {
       const optionss = catagories.map((category: any) => ({
         label: category.name,
+        value:category.name
       }));
 
       setOptions(optionss);
     });
   }, []);
 
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  
+
   return (
     <Space wrap>
-      
       <Select
         defaultValue="Select category"
-        style={{ width:600 }}
+        style={{ width: 200 }}
         onChange={handleChange}
         options={options}
+        
       />
     </Space>
   );
