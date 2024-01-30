8ac2d384a2a28b2fb0f61c6cca44b58565cab985
tulasi Kurete <tulasikurete410@gmail.com>
2024-01-30 12:52:50 +0530

    adding files

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,11 +1,14 @@
 import Profile from "@/components/EditProfile";
+import { Flex } from "antd";
 import Link from "next/link";
 
 export default async function Account() {
   return (
     <main>
-      <h2>User Details</h2>
-      <Link href="/account/edit">Edit Details</Link>
+      <Flex vertical>
+        <Link href="/account/userDetails">user Details</Link>
+        <Link href="/account/edit">Edit Details</Link>
+      </Flex>
     </main>
   );
 }
--- src/app/account/userDetails/page.tsx
+++ src/app/account/userDetails/page.tsx
@@ -1,22 +1,9 @@
-"use client";
-import { useCartStore } from "@/lib/cartStore";
-import { useUserStore } from "@/lib/userStore";
-import { Card, Col, Row, Typography } from "antd";
-import { useSession } from "next-auth/react";
-import React from "react";
-export default function UserDetails() {
-  
-  const user = useUserStore((s) => s.user);
-  const userAddress = useUserStore((s) => s.updateUser);
+import EditUserDetails from "@/components/EditUserDetails";
 
+export default async function Account() {
   return (
-    <Row>
-      <Col xs={24} lg={12}></Col>
-      <Col xs={24} lg={12}>
-        <Card style={{ width: 300 }}>
-          <Typography></Typography>
-        </Card>
-      </Col>
-    </Row>
+    <main>
+      <EditUserDetails />
+    </main>
   );
 }
